c58f6104bb2eec7708430c17110eab9eff96ee7b
d90375 <d90375@gmail.com>
2020-04-25 20:06:23 +0300

    init:start task

---
+++ README.md
@@ -0,0 +1,1 @@
+![Alt text](/task/gitiq_task_mk3.png?raw=true "Optional Title")
---
+++ client/components/scenes/search.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Head from '../head'
+
+const Search = () => {
+  return (
+    <div>
+      <Head title="Hello" />
+      <div className="flex items-center justify-center h-screen">
+        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+          This is222222ummy component
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Search.propTypes = {}
+
+export default Search
